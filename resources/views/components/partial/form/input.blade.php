3b89a3a6750d9c6e83bb883bdf257fdae170fc93
Alandrian Surya Tantra <alandriansuryatantra@gmail.com>
2023-12-10 19:25:21 +0700

    view: fixed desain signin dan signup serta mengedit tampilan ide.user

--- resources/views/components/partial/form/input.blade.php
+++ resources/views/components/partial/form/input.blade.php
@@ -2,8 +2,8 @@
 
 <input {!! $attributes->class(['animate-shake' => $error,' ring-red-600' => $error,' ring-2' =>$error,' block','
 w-full',' rounded-md',' border-0','
-px-2',' py-1.5',' text-gray-900',' shadow-sm',' ring-1',' ring-inset',' ring-gray-300',' placeholder:text-gray-400','
-focus:ring-2',' focus:ring-inset',' focus:ring-gray-600',' sm:text-sm',' sm:leading-6' ,'outline-none']) !!}>
+px-2',' py-1.5',' text-gray-900', ' bg-gray-100/50', ' shadow-sm',' ring-1',' ring-inset',' ring-gray-300',' placeholder:text-gray-400','
+focus:ring-2',' focus:ring-inset',' focus:ring-gray-600',' sm:text-sm',' sm:leading-6' ,'outline-none text-sm md:text-base']) !!}>
 
 @if($error)
 <ul class="text-red-500 text-xs space-y-1 list-disc list-inside mt-1.5">
